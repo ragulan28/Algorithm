d11dec69490fb8c67b30997125f82e08a7fdc33b
Ragulan <Ragulan28@gmail.com>
2018-07-09 14:14:59 +0530

    Bubble sort added

--- src/com/ragul/Sort.java
+++ src/com/ragul/Sort.java
@@ -41,6 +41,20 @@ public class Sort {
         return list;
     }
 
+    public int[] bubbleSort(int[] list) {
+        int i, j, temp = 0;
+        for (i = 0; i < list.length - 1; i++) {
+            for (j = 0; j < list.length - 1; j++) {
+                if (list[j] > list[j + 1]) {
+                    temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+
+        }
+        return list;
+    }
 
 
 
