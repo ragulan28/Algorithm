ca644a3ac83492c72eecc7daab739ca7fab5b772
Ragulan <Ragulan28@gmail.com>
2018-07-10 06:14:58 +0530

    Quick sort added

--- src/com/ragul/Main.java
+++ src/com/ragul/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 //        Sort sort = new Sort();
-        MergeSort mergeSort=new MergeSort();
+        QuickSort  quickSort=new QuickSort();
         int[] list={2,3,5,8,9,3,33,455,67,89,22,32};
 //        int[] sortedList=sort.insertionSort(list);
-        mergeSort.sort(list);
+        quickSort.sort(list);
 
         for (int number : list) {
             System.out.print(number+" ");
